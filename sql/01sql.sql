cdcbca91f433d75ca298675230e950232805fefd
dasom <zzzoo42@naver.com>
2023-10-06 09:12:08 +0900

    10월4일 데이터베이스

---
+++ sql/01sql.sql
@@ -0,0 +1,12 @@
+-- 주석
+-- ctrl 엔터 누르면 실행 시킬수있다.
+select * from w3schools.customers;
+select * from w3schools.employees;
+select *
+from w3schools.customers;
+-- sql 은 대 소문자 구분 안함
+SELECT *
+FROM W3SCHOOLS.CUSTOMERS;
+-- 키워드는 대문자, 나머지는 소문자 (관습, 엄격하지 않음)
+SELECT *
+FROM w3schools.customers;
