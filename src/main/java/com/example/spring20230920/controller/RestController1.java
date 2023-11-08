cf4403c7b6dfcb83558c8c7fb2c176991eca5a64
dasom <zzzoo42@naver.com>
2023-11-08 12:31:44 +0900

    @RestController 는 @Controller +@ResponseBody 의 결합이다

--- src/main/java/com/example/spring20230920/controller/RestController1.java
+++ src/main/java/com/example/spring20230920/controller/RestController1.java
@@ -9,19 +9,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+//@Controller
+//@ResponseBody // 모든 메소드에 적용됨
+@RestController // @Controller +@ResponseBody
 @RequiredArgsConstructor
 @RequestMapping("api/main1")
 public class RestController1 {
-
     private final MyDao10 dao;
 
     // http://localhost:8080/api/main1/sub1
     @GetMapping("sub1")
-    @ResponseBody
     public String method1() {
         return "customer name!!";
     }
@@ -30,20 +31,17 @@ public class RestController1 {
     // get 요청시
     // 5번 고객의 customerName 응답
     @GetMapping("sub2")
-    @ResponseBody
     public String method2() {
         return dao.getCustomerName();
     }
 
     @GetMapping("sub3")
-    @ResponseBody
     public MyDto34Customer method3() {
         return dao.getCustomer();
     }
 
 
     @GetMapping("sub4")
-    @ResponseBody
     public ResponseEntity<MyDto34Customer> method4(Integer id){
         MyDto34Customer customer = dao.getCustomerById(id);
 
@@ -56,7 +54,6 @@ public class RestController1 {
     }
 
     @GetMapping("sub5")
-    @ResponseBody
     public ResponseEntity<MyDto33Employee> method5(Integer id){
         MyDto33Employee employee = dao.getEmployeesById(id);
 
@@ -68,13 +65,11 @@ public class RestController1 {
     }
 
     @GetMapping("sub6")
-    @ResponseBody
     public List<Integer> method6(){
         return dao.getCustomerIdList();
     }
 
     @GetMapping("sub7")
-    @ResponseBody
     public List<Integer> method7(){
         return dao.getEmployeesIdList();
     }
